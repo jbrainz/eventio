b5a58713e18b9947fc7bb607c2e7646eb40c2157
jbrainz <developer@roberronero.com>
2021-08-30 12:41:21 +0100

    main app file container

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-function App() {
+
+import AuthState from './context/auth/AuthState'
+
+
+import Dashboard from './pages/dashboard/Dashboard';
+import CreateEvent from './pages/dashboard/CreateEvent';
+import EditEvent from './pages/dashboard/EditEvent';
+import { DashboardDetails } from './pages/dashboard/DashboardDetails';
+
+
+
+import PrivateRoutes from './pages/Routes';
+import SignUp from './pages/auth/SignUp'
+import SignIn from './pages/auth/SignIn'
+
+
+import EventState from './context/events/eventState';
+import PageNotFound from './pages/NotFound';
+
+
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AuthState>
+      <EventState>
+        <BrowserRouter>
+          <Switch>
+            <PrivateRoutes exact path="/" component={Dashboard} />
+            <PrivateRoutes path="/event/create" component={CreateEvent} />
+            <PrivateRoutes path="/event/edit/:id" component={EditEvent} />
+            <PrivateRoutes path="/event/details/:id" component={DashboardDetails} />
+            <Route path='/signup' exact component={SignUp} />
+            <Route path='/signin' exact component={SignIn} />
+            <Route component={PageNotFound} />
+          </Switch>
+        </BrowserRouter>
+      </EventState>
+    </AuthState>
+  )
 }
 
 export default App;
